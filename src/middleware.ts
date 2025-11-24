e2e77da2e50361488562b6d9ac2305111c1a3515
KAIF-99-lg <k82102002@gmail.com>
2025-11-25 00:20:02 +0530

    FireBase added

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,8 @@
-import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
+import { authMiddleware } from "@clerk/nextjs";
 
-export function middleware(request: NextRequest) {
-  return NextResponse.next();
-}
+export default authMiddleware({
+  publicRoutes: ["/"],
+});
 
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
